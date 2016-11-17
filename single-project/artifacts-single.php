82aaf67fab44849e83b426e4db1ce93be4934fd4
Phil Everson <phil@phileverson.com>
2016-11-17 10:56:10 -0500

    Greatly improved UX for coding artifacts - much less fragile. Added code to save each fragment to the fragments table when saving the artifact.

--- single-project/artifacts-single.php
+++ single-project/artifacts-single.php
@@ -98,7 +98,7 @@ abbr.editing {
 	/*border-right: 1px solid #f5f5f5 !important;*/
 }
 .artifactBody-step3-container {
-	border-top: 1px solid #f5f5f5 !important;
+	/*border-top: 1px solid #f5f5f5 !important;*/
 	border-bottom: 1px solid #f5f5f5 !important;
 }
 #addFragmentForm_Message.active {
@@ -110,6 +110,10 @@ abbr.editing {
     z-index: 10;
     display: flex;
     align-items: center;
+}
+#fragmentModalOptions .form-group {
+	margin-bottom: 10px;
+	margin-top: 10px;
 }
 </style>
 <div id="cal1">&nbsp;</div>
@@ -182,8 +186,8 @@ abbr.editing {
 												<div class="box-body omega alpha">
 												<div id="addFragmentForm_Message" class="active">
 													<div class="alert alert-info" style="width:100%; margin:20%;">
-														<h4><i class="icon fa fa-info"></i> No Fragment Selected!</h4>
-														To view coded data for a particular fragment, please select one. To code data for the first time, highlight the text you'd like to code.
+														<h4><i class="icon fa fa-info"></i> Select Fragment</h4>
+														To view coded data for a particular fragment, please select one.</br>To code data for the first time, highlight the text you'd like to code.
 													</div>
 												</div>
 													<div class="col-xs-12" style="border-bottom: 1px solid #f4f4f4">
@@ -212,20 +216,12 @@ abbr.editing {
 															</div>
 														</div>
 													</div>
-													<div class="col-xs-12">
-														<div class="form-group" style="margin-left: 0px; margin-right: 0px;">
-															<label for="fragmentModalOptions_Weight" class="col-sm-12 alpha omega">Numeric Weight:</label>
-															<div class="col-sm-12 alpha omega">
-																<input type="text" class="form-control" id="fragmentModalOptions_Weight" placeholder="Enter a number"></input>
-															</div>
-														</div>
-													</div>
-												</div>
-												<div class="box-footer">
-													<button type="button" id="fragmentModalOptions_Save" class="btn btn-sm btn-primary">Save Fragment</button>
-													<button type="reset" id="fragmentModalOptions_Cancel" class="btn btn-sm btn-default">Cancel</button>
 												</div>
 											</form>
+											<div class="box-footer">
+												<button type="" id="saveFragment" class="btn btn-xs btn-primary">Save Fragment</button>
+												<button type="" id="deleteFragment" class="btn btn-xs btn-warning">Delete Fragment</button>
+											</div>
 										</div>
 									</div>
 								</div>
@@ -238,12 +234,40 @@ abbr.editing {
 				</form>
 			</div>
 			<div class="box-footer">
-				<button type="" id="saveCodingButton" class="btn btn-primary" >Save Artifact & Coded Fragments</button>
+				<button type="" id="saveCodingButton" class="btn btn-primary">Save Artifact & Coded Fragments</button>
 				<a type="reset" id="editArtifactButton_Cancel" class="btn btn-default">Cancel</a>
 			</div>
 		</div>
 	</div>
 </div>
+
+<!-- Modal -->
+<div class="modal" id="saveArtifactModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        <h4 class="modal-title" id="myModalLabel">Saving Artifact</h4>
+      </div>
+      <div class="modal-body">
+
+        <strong>Saving Process</strong>
+		<div class="progress">
+			<div id="savingExecutionProgressBar" class="progress-bar progress-bar-primary progress-bar-striped" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100"></div>
+		</div>
+        <strong>Saving Execution History</strong>
+        <ul id="savingExecutionHistory">
+        	
+        </ul>
+      </div>
+      <div class="modal-footer">
+        <!-- <button type="button" class="btn btn-default" data-dismiss="modal">Close</button> -->
+        <a id="saveArtifactModal_ReturnLink" type="button" href="" class="btn btn-primary">Continue to Artifacts & Coding Dashboard</a>
+      </div>
+    </div>
+  </div>
+</div>
+
 
 <script type="text/javascript">
 	// Elements:
@@ -261,8 +285,8 @@ abbr.editing {
 	var fragmentModalOptions_NodeB = $('#fragmentModalOptions_NodeB');
 	var fragmentModalOptions_Feelings = $('#fragmentModalOptions_Feelings');
 	var fragmentModalOptions_Comment = $('#fragmentModalOptions_Comment');
-	var fragmentModalOptions_Save = $('#fragmentModalOptions_Save');
-	var fragmentModalOptions_Cancel = $('#fragmentModalOptions_Cancel');
+
+	var savingExecutionHistory = $('#savingExecutionHistory');
 
 	var allCodes = [];
 	var allNodes = [];
@@ -272,6 +296,7 @@ abbr.editing {
 	var latestArtifactBodyContents = '';
 
 	$('#editArtifactButton_Cancel').attr('href', '/single-project/?projectID=' + currentProject + '&destination=artifacts');
+	$('#saveArtifactModal_ReturnLink').attr('href', '/single-project/?projectID=' + currentProject + '&destination=artifacts');
 
 
 	// *************** All Nodes for Project ***************
@@ -428,10 +453,10 @@ abbr.editing {
 	}
 	function endStep1() {
 		console.log('END STEP 1');
-		var bodyContents = CKEDITOR.instances.artifactBody.getData();
-		if (CKEDITOR.instances.artifactBody) CKEDITOR.instances.artifactBody.destroy();
-		performBasicSave(bodyContents);
-		latestArtifactBodyContents = bodyContents;
+		if (CKEDITOR.instances.artifactBody) {
+			latestArtifactBodyContents = CKEDITOR.instances.artifactBody.getData();
+			CKEDITOR.instances.artifactBody.destroy();
+		}
 	}
 	function initStep2() {
 		console.log('INIT STEP 2');
@@ -446,7 +471,6 @@ abbr.editing {
 		latestArtifactBodyContents = $('#artifactBodyForWordFragmenting').html();
 		console.log(latestArtifactBodyContents);
 		console.log($('#artifactBodyForWordFragmenting').html());
-		performArtifactWithCodesSave();
 	}
 	function initStep3() {
 		console.log('INIT STEP 3');
@@ -455,13 +479,12 @@ abbr.editing {
 		$('#artifactBodyForCoding').append(latestArtifactBodyContents);
 		activateFragmentCoding();
 		$('abbr').click(function(e){
-			showAbbrOptions(e.target.id);
+			showFragmentOptionsModal(e.target.id);
 		})
 	}
 	function endStep3() {
 		console.log('END STEP 3');
 		latestArtifactBodyContents = $('#artifactBodyForCoding').html();
-		performArtifactWithCodesSave()
 	}
 
 
@@ -515,6 +538,30 @@ abbr.editing {
 		}
 	}
 
+	function checkIfPartOfFragmentWindow(element) {
+		var currentCheckingElement = $(element).parent();
+		var fragmentModalOptions = $('#fragmentModalOptions');
+		var contentWindow = $('#fragmentModalOptions');
+		console.log('comparing to:');
+		console.log(fragmentModalOptions);
+
+
+		for (var i = 0; i < 10; i++) {
+			if (!currentCheckingElement[0]) {
+				return false;
+			}
+			if (currentCheckingElement[0] == fragmentModalOptions[0]) {
+				console.log("success!!!!!!!!!!!!!!!!!!");
+				return true;
+			} else {
+				console.log(currentCheckingElement[0].id);
+				currentCheckingElement = currentCheckingElement.parent();
+			}
+		}
+		console.log('reached end, the last element tried was^.');
+		return false;
+	}
+
 	function activateFragmentCoding() {
 		var ele = document.getElementById('tooltip');
 		var sel = window.getSelection();
@@ -523,6 +570,17 @@ abbr.editing {
 		var rel2= document.createRange();
 		rel2.selectNode(document.getElementById('cal2'));
 		// $('.artifactBody-Codeable').mouseup(function () {
+
+		// If we're clicking outside, and we're working on a fragment, save the fragment we're working on
+		$(document).mousedown(function (e) {
+			var anythingBeingEdited = $('body').find('abbr').hasClass('editing');
+			if (anythingBeingEdited) {
+				if (!checkIfPartOfFragmentWindow(e.toElement)) {
+					saveFragmentOptionsModal(true);
+				}
+			}
+		});
+		// Catching any time we highlight a piece of text in the artifact.
 		$(document).mouseup(function () {
 		    // if (!sel.isCollapsed) {
 		    if (!sel.isCollapsed && checkIfInArticleBody(sel)) {
@@ -542,8 +600,6 @@ abbr.editing {
 	}
 
 	codeFragmentMoveableButton.click(function(){
-		   
-		clearAnyEditingAbbrs()
 		var selection = getSelectedText();
 	    var selection_text = selection.toString();
 	    
@@ -559,10 +615,10 @@ abbr.editing {
 			console.log('hovering');
 		})
 	    $('#' + span.id).click(function(){
-			showAbbrOptions(span.id);
+			showFragmentOptionsModal(span.id);
 		})
 
- 		showAbbrOptions(span.id);
+ 		showFragmentOptionsModal(span.id);
 		$('#tooltip').css('display', 'none');
 	})
 
@@ -571,7 +627,7 @@ abbr.editing {
 	  return t;
 	}
 
-	function showAbbrOptions(abbrID) {
+	function showFragmentOptionsModal(abbrID) {
 		$('#addFragmentForm_Message').css('display', 'none');
 
 		console.log(abbrID);
@@ -622,7 +678,7 @@ abbr.editing {
 		}
 	}
 
-	fragmentModalOptions_Save.click(function(){
+	function saveFragmentOptionsModal(closeModal) {
 		var fragmentID = fragmentModalOptions.attr('data-abbrID');
 
 		var fragmentCodesFormatted = '';
@@ -641,15 +697,26 @@ abbr.editing {
 		$('#artifactBodyForCoding').find("#" + fragmentID).attr('data-feelings',fragmentModalOptions_Feelings.val());
 		$('#artifactBodyForCoding').find("#" + fragmentID).attr('data-comments',fragmentModalOptions_Comment.val());
 
-		clearAbbrOptions(fragmentID);
-	})
+		if (closeModal) {
+			clearFragmentOptionsModal(fragmentID);
+		}
+	}
 
-	fragmentModalOptions_Cancel.click(function(){
+	$('#saveFragment').click(function(e){
+		e.preventDefault();
+		saveFragmentOptionsModal(true);
+	});
+
+	$('#deleteFragment').click(function(e){
+		e.preventDefault();
 		var fragmentID = fragmentModalOptions.attr('data-abbrID');
-		clearAbbrOptions(fragmentID);
-	})
+		saveFragmentOptionsModal(true);
+		var fragmentInBody = $('#artifactBodyForCoding').find("#" + fragmentID);
+		fragmentInBody.after(fragmentInBody.text());
+		fragmentInBody.remove();
+	});
 
-	function clearAbbrOptions(abbrID) {
+	function clearFragmentOptionsModal(abbrID) {
 		$('#artifactBodyForCoding').find("#" + abbrID).removeClass('editing');
 		$('#addFragmentForm_Message').css('display', 'flex');
 		fragmentModalOptions_Codes.select2().val('').trigger("change");
@@ -664,14 +731,45 @@ abbr.editing {
 	}
 
 
+	// *************** Save Artifact (including basic details, coded html, etc.) ***************
 	saveCodingButton.click(function(){
-		performBasicSave($('#artifactBodyForCoding').html(), true);
+		$('#saveArtifactModal').modal('show');
+		savingExecutionHistory.append('<li>Requesting Save</li>');
+		// Are we actively editing a fragment?
+		var anythingBeingEdited = $('body').find('abbr').hasClass('editing');
+		if (anythingBeingEdited) {
+			saveFragmentOptionsModal(false);
+			clearAnyEditingAbbrs();
+		}
+		console.log(currentTab);
+		if (currentTab == 3) {
+			endStep3();
+			savingExecutionHistory.append('<li>Completed Tab 3 Save Process</li>');
+		} else if (currentTab == 2) {
+			endStep2();
+			savingExecutionHistory.append('<li>Completed Tab 2 Save Process</li>');
+		} else {
+			endStep1();
+			savingExecutionHistory.append('<li>Completed Tab 1 Save Process</li>');
+		}
+		performArtifactSave(false);
 	})
 
+	var savingTotalSteps = 1;
+	var savingCompletedSteps = 0;
+	function updateCompletedStepsByOne() {
+		savingCompletedSteps++;
+		console.log(savingCompletedSteps + ' / ' + savingTotalSteps);
+		var progressPercent = (savingCompletedSteps / savingTotalSteps) * 100;
+		$('#savingExecutionProgressBar').css('width', progressPercent + '%');
+	}
 
-	// *************** BASIC Artifact Save ***************
-	function performBasicSave(body, redirectHome) {
+	function performArtifactSave(redirectHome) {
 		var artifactID = gup('rid');
+
+		// Determine how many steps we have to save...
+		var allFragmentsInBody = $(latestArtifactBodyContents).find('abbr');
+		savingTotalSteps = savingTotalSteps + allFragmentsInBody.length;
 
 		var globalCodesFormatted = '';
 		if ($('#globalArtifactCodes').val()) {
@@ -687,7 +785,7 @@ abbr.editing {
 			name: $('#artifactName').val(),
 			default_projector: $('#deafultProjectingNode').val(),
 			related_project: currentProject,
-			body: body,
+			body: latestArtifactBodyContents,
 			global_codes: globalCodesFormatted
 		}
 
@@ -697,55 +795,56 @@ abbr.editing {
 			$.put("/api/standard.php/artifacts/" + artifactID, formData, function(result) {
 				if(!isNaN(result)) {
 					console.log("SUCCESS: Existing Artifact Saved");
-					if(redirectHome) {
-						window.location.replace("/single-project/?projectID=92&destination=artifacts");
-					}
+					savingExecutionHistory.append('<li>All raw artifact data saved successfully.</li>');
+					updateCompletedStepsByOne();
 				} else {
-					console.log('Error adding node.');
+					console.log('Error saving artifact.');
+					savingExecutionHistory.append('<li>Error: Raw Artifact Data</li>');
 				}
 			});
 		} else {
 			$.post("/api/standard.php/artifacts", formData, function(result) {
 				if(!isNaN(result)) {
 					console.log("SUCCESS: Artifact Saved");
-					if(redirectHome) {
-						window.location.replace("/single-project/?projectID=92&destination=artifacts");
-					}
+					savingExecutionHistory.append('<li>All raw artifact data saved successfully.</li>');
+					updateCompletedStepsByOne();
 				} else {
-					console.log('Error adding node.');
+					console.log('Error saving artifact.');
+					savingExecutionHistory.append('<li>Error: Raw Artifact Data</li>');
 				}
 			});
 		}
+
+		// @TODO: Mass delete any fragments in the fragments table for this artifact, than re add them (you'll need a separate function)
+
+		// Save individual fragments
+		for (var i = 0; i < allFragmentsInBody.length; i++) {
+			saveSingleFragmentToDB(allFragmentsInBody[i]);
+		}
+		// if(redirectHome) {
+		// 	window.location.replace("/single-project/?projectID=92&destination=artifacts");
+		// }
 	}
 
-
-	// *************** Artifact With Codes Save ***************
-	function performArtifactWithCodesSave() {
-		var artifactID = gup('rid');
-		var formData = {
-			body: $('#artifactBodyForCoding').html()
+	function saveSingleFragmentToDB(fragmentElement) {
+		var fragmentElementFormData = {
+			related_artifact: gup('rid'),
+			inline_id: $(fragmentElement).attr('id'),
+			codes: $(fragmentElement).attr('data-codes'),
+			nodeA: $(fragmentElement).attr('data-nodeA'),
+			nodeB: $(fragmentElement).attr('data-nodeB'),
+			feeling: $(fragmentElement).attr('data-feelings'),
+			comments: $(fragmentElement).attr('data-comments')
 		}
-
-		// @TODO: Error Validation
-
-		if (artifactID) {
-			$.put("/api/standard.php/artifacts/" + artifactID, formData, function(result) {
-				if(!isNaN(result)) {
-					console.log("SUCCESS: Codes for Artifact Saved");
-					// alert("Success: Codes Saved");
-				} else {
-					console.log('Error saving artifact codes.');
-				}
-			});
-		}
+		$.post("/api/standard.php/fragments", fragmentElementFormData, function(result) {
+			if(!isNaN(result)) {
+				console.log("SUCCESS: Fragment Element Saved");
+				savingExecutionHistory.append('<li>Fragment #' + fragmentElementFormData.inline_id + ' saved successfully.</li>');
+				updateCompletedStepsByOne();
+			} else {
+				console.log('Error saving fragment.');
+				savingExecutionHistory.append('<li>Error Saving Fragment #' + fragmentElementFormData.inline_id + '.</li>');
+			}
+		});
 	}
-
-	// function disableBasicFields() {
-	// 	globalArtifactCodes.attr('disabled', 'true');
-	// 	deafultProjectingNode.attr('disabled', 'true');
-	// 	$('#artifactName').attr('disabled', 'true');
-	// 	beginCodingButton.attr('disabled', 'true');
-	// 	saveCodingButton.removeAttr('disabled');
-	// }
-
 </script>
